5713231428f6d72b8707885d19bfcc9fd8ab379b
Isaac Colls <colls_isaac@hotmail.com>
2020-02-17 13:30:02 -0300

    Fetch data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import MovieList from './components/movie-list';
 
 class App extends Component {
-  movies = ['titanic', 'avatar'];
+
+  constructor(props) {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
 
   componentDidMount() {
-    
+    fetch('http://127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 934198eddddfe5a40d6c8ddc04400b5af2cb348f'
+      }
+    })
+      .then(resp => resp.json())
+      .then(res => {
+        console.log(res);
+        this.setState({movies: res})
+      })
+      .catch(error => console.log(error))
   }
 
   render() {
     return (
       <div className="App">
         <h1>Movie Rater</h1>
-        <MovieList movies={this.movies} />
+        <MovieList movies={this.state.movies} />
       </div>
     );
   }
--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -5,7 +5,7 @@ function MovieList(props) {
     return (
         <React.Fragment>
             { props.movies.map(movie => {
-                return <h3>{movie}</h3>
+                return <h3 key={movie.id}>{movie.title}</h3>
             })}
         </React.Fragment>
     );
