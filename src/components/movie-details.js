3ab15ecb911c4c62c9686025396da3267d8a697f
Isaac Colls <colls_isaac@hotmail.com>
2020-02-18 11:08:23 -0300

    rating

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class MovieDetails extends Component {
     render() {
+        const mov = this.props.movie;
+
         return (
             <React.Fragment>
-                { this.props.movie ? (
+                { mov ? (
                     <div>
-                        <h3>{this.props.movie.title}</h3>
-                        <p>{this.props.movie.description}</p>
+                        <h3>{mov.title}</h3>
+                        <FontAwesomeIcon icon={["fas","star"]} className={mov.avg_rating > 0 ? 'orange' : ''} />
+                        <FontAwesomeIcon icon={["fas","star"]} className={mov.avg_rating > 1 ? 'orange' : ''} />
+                        <FontAwesomeIcon icon={["fas","star"]} className={mov.avg_rating > 2 ? 'orange' : ''} />
+                        <FontAwesomeIcon icon={["fas","star"]} className={mov.avg_rating > 3 ? 'orange' : ''} />
+                        <FontAwesomeIcon icon={["fas","star"]} className={mov.avg_rating > 4 ? 'orange' : ''} />
+                        ({mov.no_of_ratings})
+                        <p>{mov.description}</p>
                     </div>
                 ) : null }
             </React.Fragment>
