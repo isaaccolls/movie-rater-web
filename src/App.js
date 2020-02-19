bf6017adfce2eb906092c3c7282ad7dd7b64d948
Isaac Colls <colls_isaac@hotmail.com>
2020-02-19 12:27:21 -0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,101 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Login from './components/login';
+import Movies from './components/movies';
 import './App.css';
-import MovieList from './components/movie-list';
-import MovieDetails from './components/movie-details';
-import MovieForm from './components/movie-form';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class App extends Component {
+function App() {
 
-  constructor(props) {
-    super();
-    this.state = {
-      movies: [],
-      selectedMovie: null,
-      editedMovie: null,
-    };
-  }
-
-  componentDidMount() {
-    fetch(`${process.env.REACT_APP_API_URL}/api/movies/`, {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Token 8c667c2fa7048eb4d07aeca5e650b3757ce29220'
-      }
-    })
-      .then(resp => resp.json())
-      .then(res => {
-        console.log(res);
-        this.setState({movies: res})
-      })
-      .catch(error => console.log(error))
-  }
-
-  loadMovie = movie => {
-    // console.log(movie);
-    this.setState({selectedMovie: movie, editedMovie: null});
-  }
-
-  movieDeleted = selMovie => {
-    const movies = this.state.movies.filter(movie => movie.id !== selMovie.id);
-    this.setState({movies: movies, selectedMovie: null});
-  }
-
-  editClicked = selMovie => {
-    // console.log("edit", selMovie);
-    this.setState({editedMovie: selMovie});
-  }
-
-  newMovie = () => {
-    // console.log("newMovie");
-    this.setState({
-      editedMovie: {
-        title: '',
-        description: '',
-      }
-    })
-  }
-
-  cancelForm = () => {
-    this.setState({editedMovie: null});
-  }
-
-  addedMovie = movie => {
-    this.setState({movies: [...this.state.movies, movie]});
-  }
-
-  render() {
     return (
-      <div className="App">
-        <h1>
-          <FontAwesomeIcon icon={["fas","film"]} />
-          <span>Movie Rater</span>
-        </h1>
-        <div className="layout">
-          <MovieList
-            movies={this.state.movies}
-            movieClicked={this.loadMovie}
-            movieDeleted={this.movieDeleted}
-            editClicked={this.editClicked}
-            newMovie={this.newMovie}
-          />
+        <BrowserRouter>
           <div>
-            { !this.state.editedMovie ?
-              <MovieDetails
-                movie={this.state.selectedMovie}
-                updateMovie={this.loadMovie}
-              /> :
-              <MovieForm
-                movie={this.state.editedMovie}
-                cancelForm={this.cancelForm}
-                newMovie={this.addedMovie}
-                editedMovie={this.loadMovie}
-              /> }
+            <Route exact path="/" component={Login} />
+            <Route exact path="/movies" component={Movies} />
           </div>
-        </div>
-      </div>
+        </BrowserRouter>
     );
   }
-}
 
 export default App;
---
+++ src/components/movies.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import MovieList from './movie-list';
+import MovieDetails from './movie-details';
+import MovieForm from './movie-form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Movies extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+      editedMovie: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_API_URL}/api/movies/`, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 8c667c2fa7048eb4d07aeca5e650b3757ce29220'
+      }
+    })
+      .then(resp => resp.json())
+      .then(res => {
+        console.log(res);
+        this.setState({movies: res})
+      })
+      .catch(error => console.log(error))
+  }
+
+  loadMovie = movie => {
+    // console.log(movie);
+    this.setState({selectedMovie: movie, editedMovie: null});
+  }
+
+  movieDeleted = selMovie => {
+    const movies = this.state.movies.filter(movie => movie.id !== selMovie.id);
+    this.setState({movies: movies, selectedMovie: null});
+  }
+
+  editClicked = selMovie => {
+    // console.log("edit", selMovie);
+    this.setState({editedMovie: selMovie});
+  }
+
+  newMovie = () => {
+    // console.log("newMovie");
+    this.setState({
+      editedMovie: {
+        title: '',
+        description: '',
+      }
+    })
+  }
+
+  cancelForm = () => {
+    this.setState({editedMovie: null});
+  }
+
+  addedMovie = movie => {
+    this.setState({movies: [...this.state.movies, movie]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <FontAwesomeIcon icon={["fas","film"]} />
+          <span>Movie Rater</span>
+        </h1>
+        <div className="layout">
+          <MovieList
+            movies={this.state.movies}
+            movieClicked={this.loadMovie}
+            movieDeleted={this.movieDeleted}
+            editClicked={this.editClicked}
+            newMovie={this.newMovie}
+          />
+          <div>
+            { !this.state.editedMovie ?
+              <MovieDetails
+                movie={this.state.selectedMovie}
+                updateMovie={this.loadMovie}
+              /> :
+              <MovieForm
+                movie={this.state.editedMovie}
+                cancelForm={this.cancelForm}
+                newMovie={this.addedMovie}
+                editedMovie={this.loadMovie}
+              /> }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movies;
